0636c29ac1b83af3500e869f00324ac7ec8dec51
chekaaa <carlos93@hotmail.cl>
2018-10-20 19:06:26 -0300

    New track shape and collision

---
+++ Assets/Resources/Scripts/TacksBoundTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacksBoundTest : MonoBehaviour
+{
+    public Transform respawnPosition;
+
+    private void Awake()
+    {
+        respawnPosition = GameObject.Find("SpawnPoint").transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision");
+
+        if (other.transform.tag == "Bound")
+        {
+            transform.position = respawnPosition.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("@OnTriggerEnter2D");
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/TrackBoundChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBoundChecker : MonoBehaviour
+{
+    private const string CHECKPOINT_TAG = "Checkpoint";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == CHECKPOINT_TAG)
+        {
+            Debug.Log(other.transform.name);
+        }
+    }
+
+}
